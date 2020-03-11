d9918ad72da1e21a089f9f10cc4d7925e11ebb90
Daehoon Song <dhsong@Song-MacBookPro.local>
2020-03-11 16:40:46 +0900

    Book Database Modeling

--- datamodeling/books/models.py
+++ datamodeling/books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    isbn = models.IntegerField(unique=True)
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    pub_date = models.DateField()
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    books = models.ManyToManyField('Book')
+
+    def __str__(self):
+        return self.name
